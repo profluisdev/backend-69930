894250041479f425ad226edcde8b8f6cae27a3f3
Luis Mera <profeluismeradev@gmail.com>
2024-05-23 20:00:52 -0300

    chat

---
+++ clase-11-Aplicación chat con Websockets/public/js/index.js
@@ -0,0 +1,47 @@
+
+
+const socket = io();
+
+let user;
+let chatBox = document.getElementById("chatBox");
+
+Swal.fire({
+  title: "Indentificate",
+  input: "text",
+  text: "Ingresar el usuario para identificarte en el chat",
+  inputValidator: (value) => {
+    return !value && "Por favor ingrese el nombre de usuario"
+  },
+  allowOutsideClick: false,
+}).then( result => {
+  user = result.value;
+  socket.emit("newUser", user);
+})
+
+chatBox.addEventListener("keydown", (event) => {
+  if(event.key === "Enter") {
+    if(chatBox.value.trim().length > 0) {
+      socket.emit("message", {user: user, message: chatBox.value});
+      chatBox.value = "";
+    } else {
+      alert("Debe escribir al menos un carácter")
+    }
+  } 
+  
+})
+
+
+socket.on("messageLog", (data) => {
+  console.log(data);
+})
+
+socket.on("newUser", (data) => {
+
+  Swal.fire({
+    text: `Usuario ${data} conectado`,
+    toast: true,
+    position: "top-right",
+    timer: 3000
+  })
+  
+})
---
+++ clase-11-Aplicación chat con Websockets/src/app.js
@@ -0,0 +1,39 @@
+import express from "express";
+import handlebars from "express-handlebars";
+import _dirname from "./dirname.js";
+import viewRoutes from "./routes/views.routes.js";
+import { Server } from "socket.io";
+
+const PORT = 8081;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Configuración de handlebars
+app.engine("handlebars", handlebars.engine()); // Inicia el motor del la plantilla
+app.set("views", _dirname + "/views"); // Indicamos que ruta se encuentras las vistas
+app.set("view engine", "handlebars"); // Indicamos con que motor vamos a utilizar las vistas
+app.use(express.static("public"));
+app.use("/", viewRoutes);
+
+const httpServer = app.listen(PORT, () => {
+  console.log(`Server on port ${PORT}`);
+});
+
+let messages = [];
+
+const socketServer = new Server(httpServer);
+
+socketServer.on("connection", (socket) => {
+  console.log("Nuevo usuario conectado");
+  socket.on("message", (data) => {
+      messages.push(data);
+      console.log(messages);
+      socketServer.emit("messageLog", messages);
+  })
+
+  socket.on("newUser", (data) => {
+      socket.broadcast.emit("newUser", data);
+  })
+});
